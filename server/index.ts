c091a97656267fb90b1329915fda283cd029a435
Kav-Senpai <106959520+Kav-Senpai@users.noreply.github.com>
2025-07-18 15:12:58 +0530

    deploying server

--- server/index.ts
+++ server/index.ts
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 4000;
+const PORT = process.env.PORT || 5000;
 
 connectDb();
 app.use(cors({
@@ -113,5 +113,5 @@ app.get('/api/product-image/:skuId', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('app is running at port: ', PORT);
+  console.log(`Server running on port ${PORT}`);
 }); 
