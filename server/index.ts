a2f2bf9618f71bf8df29f12d046d8cc2d947e795
Aryan <aryangupta07075@gmail.com>
2025-07-18 17:32:13 +0530

    update

--- server/index.ts
+++ server/index.ts
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 4000;
 
 connectDb();
 app.use(cors({
@@ -112,6 +112,6 @@ app.get('/api/product-image/:skuId', (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(4000, () => {
+  console.log(`Server running on port ${4000}`);
 }); 
